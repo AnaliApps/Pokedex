56db3ad84a450f7bee1b2ceae82203169106982e
AnaliApps <ronoanali5@gmail.com>
2024-08-08 21:20:28 +0300

    Finished touches complete
    - App now is finalized and works as the other pokemon apps

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         "bundle":'./src/index.js',
         "detail": './src/pokemonDetail.js',
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: true,
+            chunks: ["bundle"],
+            filename:"index.html"
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/detail.html',
+            inject: true,
+            chunks: ["detail"],
+            filename:"detail.html"
+        }),
+    ],
     output: {
         filename:'[name].js',
         path:path.resolve(__dirname,'dist'),
+        clean:true,
     },
     module:{
         rules:[
