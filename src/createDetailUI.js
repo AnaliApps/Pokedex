4d1791d4d111513030e2148a9e3fe448e921812e
AnaliApps <ronoanali5@gmail.com>
2024-08-09 12:37:08 +0300

    Add styling for small screens
    - I have added styling for small screens.

--- src/createDetailUI.js
+++ src/createDetailUI.js
@@ -8,18 +8,30 @@ export default function createDetailUI(){
     let img = document.createElement('img');
     img.setAttribute('class','sprite-img')
     imgDiv.appendChild(img)
+    let leftDiv = document.createElement('div');
+    leftDiv.setAttribute('class',"leftDiv")
     let leftBtn = document.createElement('button');
     leftBtn.setAttribute('class',"left")
     leftBtn.textContent = '<'
+    leftDiv.appendChild(leftBtn);
+    let rightDiv = document.createElement('div');
+    rightDiv.setAttribute('class',"rightDiv")
     let rightBtn = document.createElement('button');
     rightBtn.setAttribute('class',"right")
     rightBtn.textContent = '>'
+    rightDiv.appendChild(rightBtn)
+    let header2Div = document.createElement('div');
+    header2Div.setAttribute('class','header2Div')
     let h2 = document.createElement('h2');
     h2.innerHTML = `Pokemon Character Stats Details`
-    statsContent.appendChild(imgDiv)
-    statsContent.appendChild(leftBtn)
-    statsContent.appendChild(rightBtn)
-    statsContent.appendChild(h2);
+    let allBtnsImgs = document.createElement('div');
+    allBtnsImgs.setAttribute('class','allbtnsimg')
+    header2Div.appendChild(h2)
+    allBtnsImgs.appendChild(leftDiv)
+    allBtnsImgs.appendChild(imgDiv)
+    allBtnsImgs.appendChild(rightDiv)
+    statsContent.appendChild(allBtnsImgs)
+    mainStats.appendChild(header2Div)
     mainStats.appendChild(statsContent)
     return mainStats
 }
